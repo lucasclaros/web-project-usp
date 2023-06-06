aa25c6f44d77fe4b0c3299ad7f4d8b6e8bfa40a1
Lucas Claros <lucas.claros@cobli.co>
2023-06-06 06:59:29 -0300

    finished about page

--- pausa-brownies/src/pages/about/About.jsx
+++ pausa-brownies/src/pages/about/About.jsx
@@ -1,5 +1,24 @@
+import React from "react";
+import InfoCard from "../components/InfoCards/InfoCard";
+import "./css/About.css";
+import PausaLogo from "../components/Logo/Logo";
+
 const About = () => {
-  return <h1>About</h1>;
+    return (
+        <InfoCard
+            header={"Sobre o Pausa (Brownies)"}
+            body={
+                <div className="shaded-text about-desc">
+                    A necessidade faz o sapo pular e tempos difíceis fazem
+                    homens fortes, mas o hábito não faz o monge. Estudante de
+                    Ciências de Computação da USP São Carlos, me vi com as
+                    contas apertando e com uma receita de família em mãos. Tento
+                    manter sempre a qualidade dos produtos pois acredito que é
+                    isso que incentiva o consumo. Desejo a todos um bom apetite.
+                </div>
+            }
+        />
+    );
 };
 
 export default About;
